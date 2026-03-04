bd451edd0b70b651bf85abfef254fc2494376674
2yngsklab <2yngsklab@gmail.com>
2026-03-04 17:22:53 +0900

    Updated README.md and renamed example dataset files for clarity

--- run_examples_docker.sh
+++ run_examples_docker.sh
@@ -6,38 +6,38 @@ mkdir -p example_out
 
 # Run Docker-based examples
 /usr/bin/time --verbose docker run \
-  --user $(id -u):$(id -g) \
-  -v ./example_data:/data \
-  -v ./example_out:/output \
-  pseudodb \
-  -sp human_chr22 \
-  -fa /data/chr22.fa \
-  -s /data/docker_sample_list_chr22.txt \
-  -o /output/docker_human_chr22_case1 \
-  -t 16 -sl > ./example_out/docker_human_chr22_case1.log 2>&1
+	--user $(id -u):$(id -g) \
+	-v ./example_data:/data \
+	-v ./example_out:/output \
+	pseudodb \
+	-sp human_chr22 \
+	-fa /data/human_chr22.fa \
+	-s /data/human_chr22_sample_list_docker.txt \
+	-o /output/human_chr22_case1_docker \
+	-t 16 -sl > ./example_out/human_chr22_case1_docker.log 2>&1
 
 /usr/bin/time --verbose docker run \
-  --user $(id -u):$(id -g) \
-  -v ./example_data:/data \
-  -v ./example_out:/output \
-  pseudodb \
-  -sp human_chr22 \
-  -fa /data/chr22.fa \
-  -s /data/docker_sample_list_chr22.txt \
-  -o /output/docker_human_chr22_case2 \
-  -db /data/chr22_dbSNP.vcf.gz \
-  -dn dbSNP \
-  -t 16 > ./example_out/docker_human_chr22_case2.log 2>&1
+	--user $(id -u):$(id -g) \
+	-v ./example_data:/data \
+	-v ./example_out:/output \
+	pseudodb \
+	-sp human_chr22 \
+	-fa /data/human_chr22.fa \
+	-s /data/human_chr22_sample_list_docker.txt \
+	-o /output/human_chr22_case2_docker \
+	-db /data/human_chr22_dbSNP.vcf.gz \
+	-dn dbSNP \
+	-t 16 > ./example_out/human_chr22_case2_docker.log 2>&1
 
 /usr/bin/time --verbose docker run \
-  --user $(id -u):$(id -g) \
-  -v ./example_data:/data \
-  -v ./example_out:/output \
-  pseudodb \
-  -sp human_chr22 \
-  -fa /data/chr22.fa \
-  -s /data/docker_sample_list_chr22.txt \
-  -o /output/docker_human_chr22_case3 \
-  -db /data/chr22_pseudoDB.vcf.gz \
-  -dn pseudoDB \
-  -t 16 > ./example_out/docker_human_chr22_case3.log 2>&1
+	--user $(id -u):$(id -g) \
+	-v ./example_data:/data \
+	-v ./example_out:/output \
+	pseudodb \
+	-sp human_chr22 \
+	-fa /data/human_chr22.fa \
+	-s /data/human_chr22_sample_list_docker.txt \
+	-o /output/human_chr22_case3_docker \
+	-db /data/human_chr22_pseudoDB.vcf.gz \
+	-dn pseudoDB \
+	-t 16 > ./example_out/human_chr22_case3_docker.log 2>&1
